d73e091a9cb5f4caee0aab05fc656e60a640621f
Taufiqzs <taufiqurrahmanzahrudinsutomo@gmail.com>
2025-12-18 22:07:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { sequelize } = require("./models");
+
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
